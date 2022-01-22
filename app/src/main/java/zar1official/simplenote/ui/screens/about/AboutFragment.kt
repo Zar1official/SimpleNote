02b144f0032c146b2fff9fd89c4ae4407d58e672
Zar <dimzar233445@gmail.com>
2022-01-22 15:47:26 +0300

    implemented some nice anims

--- app/src/main/java/zar1official/simplenote/ui/screens/about/AboutFragment.kt
+++ app/src/main/java/zar1official/simplenote/ui/screens/about/AboutFragment.kt
@@ -1,9 +1,12 @@
 package zar1official.simplenote.ui.screens.about
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
 import androidx.fragment.app.Fragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import zar1official.simplenote.R
@@ -30,8 +33,18 @@ class AboutFragment : Fragment(), Subscriber {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startAnimation()
+    }
+
     companion object {
         private const val TAG = "AboutFragment"
+        private const val ANIM_DURATION = 500L
+        private const val TRANSLATION = 100
+        private const val PROPERTY_NAME_X = "translationX"
+        private const val PROPERTY_NAME_Y = "translationY"
+
 
         @JvmStatic
         fun newInstance() = AboutFragment()
@@ -40,6 +53,36 @@ class AboutFragment : Fragment(), Subscriber {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun startAnimation() = with(binding) {
+        val imageButtonFinishX = imageButton.x
+        val imageButtonStartX = imageButtonFinishX - TRANSLATION
+        val imageButtonAnimator = ObjectAnimator.ofFloat(
+            imageButton,
+            PROPERTY_NAME_X,
+            imageButtonStartX,
+            imageButtonFinishX
+        ).apply {
+            interpolator = OvershootInterpolator()
+        }
+
+        val aboutTextFinishY = aboutText.y
+        val aboutTextStartY = aboutTextFinishY - TRANSLATION
+        val aboutTextAnimator = ObjectAnimator.ofFloat(
+            aboutText,
+            PROPERTY_NAME_Y,
+            aboutTextStartY,
+            aboutTextFinishY
+        ).apply {
+            interpolator = OvershootInterpolator()
+        }
+
+        AnimatorSet().run {
+            play(imageButtonAnimator).with(aboutTextAnimator)
+            duration = ANIM_DURATION
+            start()
+        }
     }
 
     override fun subscribeViewModel() {
